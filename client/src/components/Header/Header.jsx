f5154bd1742b4fbb31ee2779d7b3f7e235524b9f
LuckySerj <lukianchuk.s@gmail.com>
2021-10-22 10:53:39 +0300

    husky add

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './Header.scss';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+  <div>
+    <Link to='/'>Home</Link>
+    <Link to='/catalogue'>Catalogue</Link>
+    <Link to='/contacts'>Contacts</Link>
+    <Link to='/shoppingcart'>Shopping Cart</Link>
+    <Link to='/adminpanel'>Admin Panel</Link>
+    <Link to='/registration'>Registration Page</Link>
+  </div>
+);
+
+export default Header;
