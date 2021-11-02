f397ce1f43f0ac62b0fffb71eac517b4059c58de
AndriiKhomik <satvaer@gmail.com>
2021-11-02 16:06:08 +0200

    add logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,17 +1,39 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 import './Header.scss';
-import {NavLink} from "react-router-dom";
+import SearchIcon from '@mui/icons-material/Search';
+import logo from './logo.png';
 
 const Header = () => {
   return (
     <div className='header'>
-      <NavLink to='/' >Home</NavLink>
-      <NavLink to='/catalogue' >Catalogue</NavLink>
-      <NavLink to='/contacts' >Contacts</NavLink>
-      <NavLink to='/shoppingcart' >Shopping Cart</NavLink>
-      <NavLink to='/adminpanel' >Admin Panel</NavLink>
-      <NavLink to='/registration' >Registration Page</NavLink>
+      <div className="container">
+        <div className="nav-container">
+          <div className="logo">
+            <img className='logo'
+                 src={logo}
+                 alt="logo"/>
+          </div>
+          <nav className="nav">
+            <NavLink exact
+                     to='/'>Home</NavLink>
+            <NavLink exact
+                     to='/catalogue'>Catalogue</NavLink>
+            <NavLink exact
+                     to='/contacts'>Contacts</NavLink>
+            <NavLink exact
+                     to='/shoppingcart'>Shopping Cart</NavLink>
+            <NavLink exact
+                     to='/adminpanel'>Admin Panel</NavLink>
+            <NavLink exact
+                     to='/registration'>Registration Page</NavLink>
+          </nav>
+          <div className="aside-nav">
+
+          </div>
+        </div>
+      </div>
     </div>
   )
 };
