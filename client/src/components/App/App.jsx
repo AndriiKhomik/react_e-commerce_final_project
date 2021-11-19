9cc2326aaa95a009c1301e5af08b36911c5b34a8
Дмитрий Соверченко <isoverchenko@gmail.com>
2021-11-19 17:22:02 +0200

    create global wrapper for App

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
-
+// import { Reset } from 'styled-reset';
+import { ThemeProvider } from '@mui/material';
+import { useRoutes } from '../../routes';
 import './App.scss';
 import Header from '../Header';
-import { useRoutes } from '../../routes';
 import Footer from '../Footer/Footer';
+import theme from '../../services/theme/theme';
+import { StyledContainer } from './Styles';
 
 const App = () => {
   const routes = useRoutes();
 
   return (
     <>
-      <Header />
-      {routes}
-      <Footer />
+      {/* <Reset /> */}
+      <ThemeProvider theme={theme}>
+        <StyledContainer>
+          <Header />
+          {routes}
+          <Footer />
+        </StyledContainer>
+      </ThemeProvider>
     </>
   );
 };
