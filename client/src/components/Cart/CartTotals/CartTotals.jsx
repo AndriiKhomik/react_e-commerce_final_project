bd16c8d6242e720aa3dc0910d5a3a5f8f7f72ba6
Andrii <satvaer@gmail.com>
2021-11-20 10:25:08 +0200

    add input in the cart totals box

--- client/src/components/Cart/CartTotals/CartTotals.jsx
+++ client/src/components/Cart/CartTotals/CartTotals.jsx
@@ -1,7 +1,16 @@
+import { Box, Button, TextField } from '@mui/material';
+// import { Box } from '@mui/system';
 import React from 'react';
 
 const CartTotals = () => {
-  return <div>Cart Totals</div>;
+  return (
+    <Box>
+      <h1>Cart Totals</h1>
+      <p>Coupon Apply</p>
+      <TextField id='outlined-basic' variant='outlined' size='small' />
+      <Button type='submit'>Apply</Button>
+    </Box>
+  );
 };
 
 export default CartTotals;
