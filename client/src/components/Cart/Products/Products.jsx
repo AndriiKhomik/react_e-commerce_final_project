5d2b2338736883aa0f30fdeed92cdad931d3a2b9
Andrii <satvaer@gmail.com>
2021-11-24 20:00:23 +0200

    add derove icon, add some queries

--- client/src/components/Cart/Products/Products.jsx
+++ client/src/components/Cart/Products/Products.jsx
@@ -6,19 +6,23 @@ import {
   TableContainer,
   TableRow,
   FormControl,
-  Select,
   MenuItem,
+  Box,
+  useMediaQuery,
 } from '@mui/material';
 import {
-  StyledImg,
   StyledDiv,
+  StyledSelect,
+  StyledSellTotals,
   StyledTableCell,
   StyledTableCellHead,
   StyledTableRow,
 } from './Styled';
-import ebook from '../img/ereader.png';
-import paperBook from '../img/open-book.png';
-import audioBook from '../img/audio-book.png';
+import theme from '../../../services/theme/theme';
+import Ebook from '../../icons/ElBookIcon';
+import CartonBook from '../../icons/PaperBookIcon';
+import AudioBook from '../../icons/AudioBookIcon';
+import DeleteIcon from '../../icons/DeleteIcon';
 
 // Test data
 const rows = [
@@ -49,64 +53,84 @@ const rows = [
 ];
 
 const Products = () => {
-  const [img, setImg] = useState(paperBook);
+  const [img, setImg] = useState(
+    <Ebook width='24px' height='28px' fill='#000000' />,
+  );
+
+  const collapseTableColumn = useMediaQuery(theme.breakpoints.up('md'));
 
   const handleChange = (value) => {
     if (value.target.value === 1) {
-      setImg(ebook);
+      setImg(<Ebook width='24px' height='28px' fill='#000000' />);
     }
     if (value.target.value === 2) {
-      setImg(paperBook);
+      setImg(<CartonBook width='24px' height='28px' fill='#000000' />);
     }
     if (value.target.value === 3) {
-      setImg(audioBook);
+      setImg(<AudioBook width='24px' height='28px' fill='#000000' />);
     }
   };
 
+  const items = rows.map((row) => (
+    <TableRow key={row.id}>
+      <StyledTableCell component='th' scope='row'>
+        {row.cover} <span>{row.number}</span>
+      </StyledTableCell>
+      {collapseTableColumn && (
+        <StyledTableCell align='center'>
+          {img}
+          <FormControl fullWidth>
+            <StyledSelect defaultValue={2} onChange={handleChange}>
+              <MenuItem value={1}>Ebook</MenuItem>
+              <MenuItem value={2}>Paper Book</MenuItem>
+              <MenuItem value={3}>Audio Book</MenuItem>
+            </StyledSelect>
+          </FormControl>
+        </StyledTableCell>
+      )}
+      {collapseTableColumn && (
+        <StyledTableCell align='center'>${row.price}</StyledTableCell>
+      )}
+      <StyledTableCell align='center' sx={{ padding: '4px' }}>
+        <StyledDiv>-</StyledDiv>
+        {row.quantity}
+        <StyledDiv sx={{ padding: '0 6px' }}>+</StyledDiv>
+      </StyledTableCell>
+      <StyledSellTotals align='center'>
+        ${row.price * row.quantity}
+      </StyledSellTotals>
+      <StyledTableCell align='center'>
+        <Box sx={{ cursor: 'pointer' }}>
+          <DeleteIcon width='18px' height='18px' />
+        </Box>
+      </StyledTableCell>
+    </TableRow>
+  ));
+
   return (
-    <TableContainer>
-      <Table sx={{ minWidth: 320 }} size='small' aria-label='a dense table'>
-        <TableHead>
+    <TableContainer sx={{ overflowY: 'hidden' }}>
+      <Table
+        sx={{ minWidth: 320, borderCollapse: 'separate', minHeight: '100%' }}
+        size='small'
+        aria-label='a dense table'
+      >
+        <TableHead sx={{ borderCollapse: 'separate' }}>
           <StyledTableRow>
             <StyledTableCellHead align='left'>Products</StyledTableCellHead>
-            <StyledTableCellHead align='center'>Format</StyledTableCellHead>
-            <StyledTableCellHead align='center'>Price</StyledTableCellHead>
-            <StyledTableCellHead align='center'>Quantity</StyledTableCellHead>
+            {collapseTableColumn && (
+              <StyledTableCellHead align='center'>Format</StyledTableCellHead>
+            )}
+            {collapseTableColumn && (
+              <StyledTableCellHead align='center'>Price</StyledTableCellHead>
+            )}
+            <StyledTableCellHead sx={{ minWidth: '70px' }} align='center'>
+              Quantity
+            </StyledTableCellHead>
             <StyledTableCellHead align='center'>Total</StyledTableCellHead>
+            <StyledTableCellHead align='center'>Delete</StyledTableCellHead>
           </StyledTableRow>
         </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <StyledTableCell component='th' scope='row'>
-                {row.cover} <span>{row.number}</span>
-              </StyledTableCell>
-              <StyledTableCell align='center'>
-                <StyledImg src={img} alt='ebook' />
-                <FormControl fullWidth>
-                  <Select
-                    defaultValue={2}
-                    onChange={handleChange}
-                    sx={{ height: '28px' }}
-                  >
-                    <MenuItem value={1}>Ebook</MenuItem>
-                    <MenuItem value={2}>Paper Book</MenuItem>
-                    <MenuItem value={3}>Audio Book</MenuItem>
-                  </Select>
-                </FormControl>
-              </StyledTableCell>
-              <StyledTableCell align='center'>${row.price}</StyledTableCell>
-              <StyledTableCell align='center'>
-                <StyledDiv>-</StyledDiv>
-                {row.quantity}
-                <StyledDiv sx={{ padding: '0 6px' }}>+</StyledDiv>
-              </StyledTableCell>
-              <StyledTableCell align='center'>
-                ${row.price * row.quantity}
-              </StyledTableCell>
-            </TableRow>
-          ))}
-        </TableBody>
+        <TableBody>{items}</TableBody>
       </Table>
     </TableContainer>
   );
--- client/src/components/Cart/Products/Styled.js
+++ client/src/components/Cart/Products/Styled.js
@@ -1,28 +1,34 @@
-import { TableCell, TableRow } from '@mui/material';
+import { Select, TableCell, TableRow } from '@mui/material';
 import { styled } from '@mui/material/styles';
+import theme from '../../../services/theme/theme';
 
 export const StyledTableRow = styled(TableRow)`
   border-bottom: 1px solid #f8a300;
 `;
 
-export const StyledTableCellHead = styled(TableCell)`
-  font-weight: 700;
-`;
+export const StyledTableCellHead = styled(TableCell)(() => ({
+  fontWeight: 'bold',
+  borderBottom: `1px solid ${theme.palette.primary.main}`,
+  padding: '4px',
+}));
 
 export const StyledTableCell = styled(TableCell)`
   border-bottom: none;
   background-color: #fbfbfb;
-  border-bottom: 8px solid white;
-  border-top: 8px solid white;
+  border-bottom: 4px solid white;
+  border-top: 4px solid white;
   span {
     font-weight: 700;
   }
 `;
 
-export const StyledImg = styled('img')`
-  width: 28px;
-  height: auto;
-`;
+export const StyledSellTotals = styled(TableCell)(() => ({
+  backgroundColor: '#fbfbfb',
+  borderBottom: '4px solid #FFFFFF',
+  borderTop: '4px solid #FFFFFF',
+  fontWeight: 'bold',
+  color: `${theme.palette.primary.main}`,
+}));
 
 export const StyledDiv = styled('div')`
   display: inline-block;
@@ -32,4 +38,17 @@ export const StyledDiv = styled('div')`
   padding: 0 8px;
   border-radius: 50%;
   cursor: pointer;
+  color: #ffffff;
+`;
+
+export const StyledSelect = styled(Select)`
+  height: 28px;
+  font-size: 14px;
+  margin: 4px 4px;
+  fieldset {
+    right: 5px;
+    left: 5px;
+    top: -3px;
+    bottom: 2px;
+  }
 `;
