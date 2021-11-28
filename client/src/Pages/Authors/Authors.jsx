8ed5ee3e0cba50f491fa28bdd0f23968ceab4259
Yulia Boiko <yulchitaiboldireva@gmail.com>
2021-11-28 22:17:35 +0200

    make last element in breadcrumbs as a paragraph

--- client/src/Pages/Authors/Authors.jsx
+++ client/src/Pages/Authors/Authors.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import AuthorsList from '../../components/AuthorsList/AuthorsList';
+import { authorsPageTitles } from '../../components/SectionTitles/pageTitles';
+import SectionTitles from '../../components/SectionTitles';
 
 const Authors = () => {
   return (
     <>
+      <SectionTitles titles={authorsPageTitles.slice(0, 2)} />
       <AuthorsList />
     </>
   );
--- client/src/components/SectionTitles/pageTitles.js
+++ client/src/components/SectionTitles/pageTitles.js
@@ -1,7 +1,12 @@
 /* eslint-disable prettier/prettier */
 export const pageTitles = [
-    { id: '1', title: 'Home', route: '/' },
-    { id: '2', title: 'Shop', route: '/catalogue' },
-    { id: '3', title: 'Shopping Cart', route: '/shoppingcart' },
-    { id: '4', title: 'Checkout', route: '/checkout' },
+  { id: '1', title: 'Home', route: '/' },
+  { id: '2', title: 'Shop', route: '/catalogue' },
+  { id: '3', title: 'Shopping Cart', route: '/shoppingcart' },
+  { id: '4', title: 'Checkout', route: '/checkout' },
+];
+
+export const authorsPageTitles = [
+  { id: '1', title: 'Home', route: '/' },
+  { id: '2', title: 'Authors', route: '/authors' },
 ];
