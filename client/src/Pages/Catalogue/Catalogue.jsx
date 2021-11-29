5ca7a48083277f7dfe10ac2a7a1ba56369a7f368
AndriiKhomik <59535057+AndriiKhomik@users.noreply.github.com>
2021-11-29 15:54:33 +0200

    Deploy (#32)
    * add file structure
    
    * add routing
    
    * add registration and not found pages
    
    * add client folder
    
    * add backend files (#3)
    
    * add backend files
    
    * merge server and frontend
    
    * change extension to jsx in component files
    
    * cod-style trying (#5)
    
    * cod-style trying (#6)
    
    * Cod style new (#7)
    
    * cod-style trying
    
    * style next trying
    
    * add npm packages
    
    * Create vercel.json
    
    * Update vercel.json
    
    * add build command to package.json
    
    * add redirect file
    
    * remove package-lock.json
    
    * add netlify file
    
    * remove vercel
    
    * refactor files
    
    Co-authored-by: Serhii Lukianchuk <86877894+LuckySerj@users.noreply.github.com>

---
+++ client/src/Pages/Catalogue/Catalogue.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ProductsList from '../../components/ProductsList';
+
+const Catalogue = () => {
+  return <ProductsList />;
+};
+
+export default Catalogue;
---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './Footer.scss';
+
+const Footer = () => {
+  return <div className='footer'>Footer</div>;
+};
+
+export default Footer;
