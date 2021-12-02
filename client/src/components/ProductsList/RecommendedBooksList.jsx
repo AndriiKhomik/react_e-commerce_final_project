22b9390968375fed23ec042063536f93b50d0f7e
Olena Savchenko <olsavchenko1@gmail.com>
2021-12-03 01:02:55 +0200

    fix books fetch on main page

--- client/src/components/ProductsList/RecommendedBooksList.jsx
+++ client/src/components/ProductsList/RecommendedBooksList.jsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import { items } from './products';
+import React, { useState, useEffect } from 'react';
+import { getProductsByQuery } from '../../api/products';
 import BooksList from './BooksList';
 
 const RecommendedBooksList = () => {
-  return <BooksList items={items} text='Recommended' />;
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getProductsByQuery('isRecommended')
+      .then((data) => {
+        setProducts(data);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  return isLoading ? (
+    'Loading'
+  ) : (
+    <BooksList items={products} text='Recommended' />
+  );
 };
 
 export default RecommendedBooksList;
