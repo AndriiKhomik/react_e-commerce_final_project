ed07e760c48a2f997af01e6fffcb9be2ea5b88a1
AndriiKhomik <59535057+AndriiKhomik@users.noreply.github.com>
2021-12-07 10:18:00 +0200

    Dev (#50)
    * Books format tags, slider links and buttons (#48)
    
    * add base catalogue structure
    
    * add main styles
    
    * fix footer and card styles
    
    * change ProductItem structure and styles
    
    * fixed Footer src in App
    
    * add base booklists styles
    
    * add book lists
    
    * fix footer and booklist styles
    
    * fix styles
    
    * checkout base styles
    
    * save order form (btn submit does not work)
    
    * add working order form button
    
    * fix books fetch on main page
    
    * fix slider
    
    * fix slider nav btns
    
    * remove unused styles
    
    * add correct propTypes at ProductImg
    
    * add flag to build command
    
    Co-authored-by: OlenaSavchenko <55785591+OlenaSavchenko@users.noreply.github.com>

---
+++ client/src/components/ProductItem/BookFormatTag/Styles.js
@@ -0,0 +1,13 @@
+import { styled } from '@mui/material/styles';
+import theme from '../../../services/theme/theme';
+
+export const StyledFormatParagraph = styled('p')(() => ({
+    position: 'absolute',
+    zIndex: '2',
+    bottom: '15px',
+    right: '8px',
+    padding: '5px',
+    backgroundColor: [theme.palette.text.light],
+    color: [theme.palette.primary.dark],
+    borderRadius: '4px',
+}));
--- client/src/components/ProductItem/BookImg/BookImg.jsx
+++ client/src/components/ProductItem/BookImg/BookImg.jsx
@@ -1,27 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import LightTooltip from '../Tooltip';
+import BookSaleTag from '../BookSaleTag';
+import BookFormatTag from '../BookFormatTag';
+import FavouriteBtn from '../FavouriteBtn';
+import LightToolTip from '../Tooltip';
 import {
   StyledCardMediaWrapper,
   StyledCardMediaBox,
   StyledCardMedia,
 } from './Styles';
 
-const ProductImg = ({ url, name }) => {
+const ProductImg = ({ url, name, categories, itemNo, salePrice }) => {
   return (
-    <LightTooltip title={name} placement='top'>
+    <LightToolTip title={name} placement='top'>
       <StyledCardMediaWrapper>
         <StyledCardMediaBox>
           <StyledCardMedia height='243' src={url} alt={name} />
         </StyledCardMediaBox>
+        <BookSaleTag salePrice={salePrice} />
+        <FavouriteBtn itemNo={itemNo} />
+        <BookFormatTag categories={categories} />
       </StyledCardMediaWrapper>
-    </LightTooltip>
+    </LightToolTip>
   );
 };
 
 ProductImg.propTypes = {
   name: PropTypes.string.isRequired,
   url: PropTypes.string.isRequired,
+  categories: PropTypes.string.isRequired,
+  itemNo: PropTypes.string.isRequired,
+  salePrice: PropTypes.number,
+};
+
+ProductImg.defaultProps = {
+  salePrice: null,
 };
 
 export default ProductImg;
