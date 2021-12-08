07afc33726e9a9e729707239d596377169cccc0b
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-12-08 22:14:39 +0200

    1

--- client/src/components/Subscribe/InfoList/items.js
+++ client/src/components/Subscribe/InfoList/items.js
@@ -1,28 +1,24 @@
 import React from 'react';
-// import AudioBookIcon from '../../icons/AudioBookIcon';
-// import ElBookIcon from '../../icons/ElBookIcon';
-// import PaperBookIcon from '../../icons/PaperBookIcon';
-
-import LaptopChromebookOutlinedIcon from '@mui/icons-material/LaptopChromebookOutlined';
-import LibraryMusicOutlinedIcon from '@mui/icons-material/LibraryMusicOutlined';
-import MenuBookOutlinedIcon from '@mui/icons-material/MenuBookOutlined';
+import AudioBookIcon from '../../icons/AudioBookIcon';
+import ElBookIcon from '../../icons/ElBookIcon';
+import PaperBookIcon from '../../icons/PaperBookIcon';
 
 export const subscribeItems = [
   {
     id: '1',
-    component: <LaptopChromebookOutlinedIcon width='75' height='75' />,
+    component: <PaperBookIcon width='75' height='75' />,
     title: 'Paper book',
     text: 'You can buy paper version of each book and enjoy it every moment',
   },
   {
     id: '2',
-    component: <LibraryMusicOutlinedIcon width='75' height='75' />,
+    component: <AudioBookIcon width='75' height='75' />,
     title: 'Audio book',
     text: 'Audio book is the best solution while you driving or in transport',
   },
   {
     id: '3',
-    component: <MenuBookOutlinedIcon width='75' height='75' />,
+    component: <ElBookIcon width='75' height='75' />,
     title: 'Electronic book',
     text: 'A lot of books and authors in you pocket device',
   },
