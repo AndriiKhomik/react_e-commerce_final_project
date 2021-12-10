67c351745688f5f2b06962de6784270f12709fa8
LuckySerj <lukianchuk.s@gmail.com>
2021-12-11 00:13:10 +0200

    merge dev

--- client/src/api/user.js
+++ client/src/api/user.js
@@ -1,11 +1,12 @@
-import { host } from './variables';
-
 export const registerUser = async (user) => {
-  const response = await fetch(`${host}/api/customers`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(user),
-  });
+  const response = await fetch(
+    `${process.env.REACT_APP_BASE_URL}/api/customers`,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(user),
+    },
+  );
   if (!response.ok) {
     throw new Error(`Error - ${response.status}`);
   }
@@ -13,11 +14,14 @@ export const registerUser = async (user) => {
 };
 
 export const loginUser = async (loginData) => {
-  const response = await fetch(`${host}/api/customers/login`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(loginData),
-  });
+  const response = await fetch(
+    `${process.env.REACT_APP_BASE_URL}/api/customers/login`,
+    {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(loginData),
+    },
+  );
   if (!response.ok) {
     // throw new Error(`Error - ${response.status}`);
     return response.json();
