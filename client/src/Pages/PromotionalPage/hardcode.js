f558f43b7a259d9d264252243eb3a5f46a483d9a
Dmytro Soverchenko <isoverchenko@gmail.com>
2021-12-17 01:48:10 +0200

    add different texts

--- client/src/Pages/PromotionalPage/hardcode.js
+++ client/src/Pages/PromotionalPage/hardcode.js
@@ -18,7 +18,7 @@ export const offer2 = {
   expTime: '15-04-2022',
   subtitle: '25% discount for all inspirational book',
   description:
-    '<ul><li>Free delivery to mini-offices and Meest post offices all over Ukraine - ordering for any amount. </li><li>Free delivery to post offices Nova Poshta all over Ukraine - ordering for any amount. </li><li>Free delivery by courier BookStore Ukrposhta, Nova Poshta or Justin - for orders over 30 USD. </li></ul>',
+    'We have been closely following your reader`s preferences throughout the 2021 year and are now ready to present you with the best of the best. In the selection you will find the top audio, e-books and paper inspiration books, which this year were in great demand among Book Store readers. So choose, order conveniently and enjoy the best.',
   terms:
     '<ul><li>The offer is valid only for orders placed during the promotion period.<li><li>Orders cannot be reissued on another day.</li><li>The BookStore reserves the right to make changes to the terms of the promotion with their mandatory posting on the site.</li><li>Details of the promotion - by phone (044) 225-0-505 or 0-800-600-505 (free from the numbers of Ukrainian operators).</li></ul>',
 };
@@ -28,11 +28,11 @@ export const offer3 = {
   imgUrl: '/images/heroBanners/banner3M.png',
   title: 'Wonderful books for children',
   expTime: '31-01-2022',
-  subtitle: '15% discount for wonderful books for childer',
+  subtitle: '15% discount for wonderful books for childen',
   description:
-    '<ul><li>Free delivery to mini-offices and Meest post offices all over Ukraine - ordering for any amount. </li><li>Free delivery to post offices Nova Poshta all over Ukraine - ordering for any amount. </li><li>Free delivery by courier BookStore Ukrposhta, Nova Poshta or Justin - for orders over 30 USD. </li></ul>',
+    'Buy promotional children books with discounts up to -15% and win the toy of Santa Claus',
   terms:
-    '<ul><li>The offer is valid only for orders placed during the promotion period.<li><li>Orders cannot be reissued on another day.</li><li>The BookStore reserves the right to make changes to the terms of the promotion with their mandatory posting on the site.</li><li>Details of the promotion - by phone (044) 225-0-505 or 0-800-600-505 (free from the numbers of Ukrainian operators).</li></ul>',
+    '<ul><li>The gift drawning will be on December 20 2021.</li><li>The winners will be determined using the Random.org service (the results will be published as a screenshot).</li><li>The offer is valid only for completed orders placed during the promotion period.</li><li>Orders cannot be reissued on another day.</li><li>The organizer reserves the right to make changes to the terms of the promotion with their mandatory posting on the site.</li></ul>',
 };
 
 export const promotionals = [
@@ -55,7 +55,7 @@ export const promotionals = [
     expTime: '15-04-2022',
     subtitle: '25% discount for all inspirational book',
     description:
-      '<ul><li>Free delivery to mini-offices and Meest post offices all over Ukraine - ordering for any amount. </li><li>Free delivery to post offices Nova Poshta all over Ukraine - ordering for any amount. </li><li>Free delivery by courier BookStore Ukrposhta, Nova Poshta or Justin - for orders over 30 USD. </li></ul>',
+      'We have been closely following your reader`s preferences throughout the 2021 year and are now ready to present you with the best of the best. In the selection you will find the top audio, e-books and paper inspiration books, which this year were in great demand among Book Store readers. So choose, order conveniently and enjoy the best.',
     terms:
       '<ul><li>The offer is valid only for orders placed during the promotion period.<li><li>Orders cannot be reissued on another day.</li><li>The BookStore reserves the right to make changes to the terms of the promotion with their mandatory posting on the site.</li><li>Details of the promotion - by phone (044) 225-0-505 or 0-800-600-505 (free from the numbers of Ukrainian operators).</li></ul>',
   },
@@ -64,10 +64,10 @@ export const promotionals = [
     imgUrl: '/images/heroBanners/banner3M.png',
     title: 'Wonderful books for children',
     expTime: '31-01-2022',
-    subtitle: '15% discount for wonderful books for childer',
+    subtitle: '15% discount for wonderful books for childen',
     description:
-      '<ul><li>Free delivery to mini-offices and Meest post offices all over Ukraine - ordering for any amount. </li><li>Free delivery to post offices Nova Poshta all over Ukraine - ordering for any amount. </li><li>Free delivery by courier BookStore Ukrposhta, Nova Poshta or Justin - for orders over 30 USD. </li></ul>',
+      'Buy promotional children books with discounts up to -15% and win the toy Santa Claus',
     terms:
-      '<ul><li>The offer is valid only for orders placed during the promotion period.<li><li>Orders cannot be reissued on another day.</li><li>The BookStore reserves the right to make changes to the terms of the promotion with their mandatory posting on the site.</li><li>Details of the promotion - by phone (044) 225-0-505 or 0-800-600-505 (free from the numbers of Ukrainian operators).</li></ul>',
+      '<ul><li>The gift drawning will be on December 20 2021.</li><li>The winners will be determined using the Random.org service (the results will be published as a screenshot).</li><li>The offer is valid only for completed orders placed during the promotion period.</li><li>Orders cannot be reissued on another day.</li><li>The organizer reserves the right to make changes to the terms of the promotion with their mandatory posting on the site.</li></ul>',
   },
 ];
