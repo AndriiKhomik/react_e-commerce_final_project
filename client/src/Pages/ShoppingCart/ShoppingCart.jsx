da6093cab42153900053a412d1c4f75aca44aa23
Andrii <satvaer@gmail.com>
2021-12-20 23:55:04 +0200

    refactor UI cart is empty and cart counter

--- client/src/Pages/ShoppingCart/ShoppingCart.jsx
+++ client/src/Pages/ShoppingCart/ShoppingCart.jsx
@@ -1,18 +1,29 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { Box } from '@mui/material';
 import CartTotals from '../../components/Cart/CartTotals/CartTotals';
 import CartProducts from '../../components/Cart/CartProducts';
 import { SalesBooksList } from '../../components/ProductsList';
 import { checkoutPageTitles } from './CheckoutpageTitles';
 import SectionTitles from '../../components/SectionTitles/SectionTitles';
-
-import { StyledBox } from './Styled';
+import { StyledBox, StyledTableTitle } from './Styled';
 
 const ShoppingCart = () => {
+  const orders = useSelector((data) => data.shoppingCart);
+
   return (
     <>
       <SectionTitles titles={checkoutPageTitles} />
       <StyledBox>
-        <CartProducts />
+        {orders.length ? (
+          <CartProducts />
+        ) : (
+          <Box sx={{ position: 'relative' }}>
+            <StyledTableTitle colSpan={5}>
+              Your shopping cart is empty
+            </StyledTableTitle>
+          </Box>
+        )}
         <CartTotals />
       </StyledBox>
       <SalesBooksList />
