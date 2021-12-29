cd6fd0e012fdbc01fe59fa9cd65f49e51e5bb172
dmitrysoverchenko <86877940+dmitrysoverchenko@users.noreply.github.com>
2021-12-30 01:07:42 +0200

    Special offer (#83)
    * add theme provider and container
    
    * working with styles
    
    * edit width of textfield
    
    * 1
    
    * 1
    
    * 1
    
    * start add styles
    
    * add styles for items and form
    
    * finish desktop style
    
    * start add media queries
    
    * finish styles and m queries
    
    * change icon ebook
    
    * 1
    
    * fix pr|
    
    * remove container
    
    * fix pr
    
    * edit padding on box 1100px res
    
    * add small mq fixes
    
    * change colors
    
    * change padding top on list
    
    * fix
    
    * 1
    
    * start one author page
    
    * add fetch for one author to api/authors.js
    
    * add section titles and working fatch
    
    * add loader
    
    * add authors photo
    
    * add bio
    
    * add date of birth
    
    * 1
    
    * correct date of birth
    
    * add button show all books
    
    * add books swiper
    
    * separate bio to bio and popular books
    
    * finished
    
    * 1
    
    * edit ways
    
    * fix max width on img component
    
    * fix img component prop type
    
    * change prop of ProductItem: remove required prop of author and add default prop as empty string
    
    * remove console log
    
    * add AuthorImg component to AuthorPage
    
    * 1
    
    * 1
    
    * fix queries and icons
    
    * edit textfield styles
    
    * 1
    
    * delete commetaries
    
    * create special offer page components
    
    * create page and routing
    
    * add hardcode object
    
    * fix-conflicts
    
    * delete lock json
    
    * 1
    
    * 1
    
    * 3
    
    * add hardcode page
    
    * add object for all promos
    
    * working with promos list
    
    * add list to promos page
    
    * add img for items
    
    * change grid
    
    * 1
    
    * good night
    
    * fix url of img
    
    * fix links
    
    * fix height of input and button:
    
    * 1
    
    * add titles
    
    * 1
    
    * render all pages by hardcode
    
    * fix media q
    
    * fix styles
    
    * 1
    
    * add different texts
    
    * img 100% and use other size L
    
    * change titles color to main colors from theme
    
    * subtitle is wrapped on paragraph
    
    * change size ot title in Promos page and remove overflow on links
    
    * change color of the items title to main and color of subtitle to primary
    
    * change column gap
    
    * start fix object-fit
    
    * 1
    
    * in progress
    
    * fix all queries
    
    * remove comments
    
    * fix promo styles, fix breadcrumbs on small screens
    
    * fix picture
    
    * fix conflicts
    
    * fix all conflicts
    
    Co-authored-by: Yulia Boiko <yulchitaiboldireva@gmail.com>

--- client/src/components/PromotionalPageComponents/PromotionalImg/Styles.js
+++ client/src/components/PromotionalPageComponents/PromotionalImg/Styles.js
@@ -1,4 +1,5 @@
 import { styled } from '@mui/material/styles';
+import { Box } from '@mui/material';
 import theme from '../../../services/theme/theme';
 
 export const StyledCardMediaBox = styled('div')(() => ({
@@ -12,12 +13,18 @@ export const StyledCardMediaBox = styled('div')(() => ({
   },
 }));
 
-export const StyledCardMedia = styled('img')(() => ({
-  [theme.breakpoints.up('xs')]: {
-    padding: '0',
-    display: 'block',
-    objectFit: 'cover',
-    borderRadius: '5px',
-    maxWidth: '100%',
-  },
-}));
+export const StyledImageWrapper = styled(Box)`
+  & img {
+    width: 100%;
+  }
+`;
+
+// export const StyledCardMedia = styled('img')(() => ({
+//   [theme.breakpoints.up('xs')]: {
+//     padding: '0',
+//     display: 'block',
+//     objectFit: 'cover',
+//     borderRadius: '5px',
+//     maxWidth: '100%',
+//   },
+// }));
