372fbf842cd76a746e6eaf8cf84c28852faa4873
Dmytro Soverchenko <isoverchenko@gmail.com>
2022-01-08 16:14:15 +0200

    add promos api

---
+++ client/src/api/promos.js
@@ -0,0 +1,15 @@
+export const getPromos = async () => {
+  const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api/slides`);
+  if (!response.ok) {
+    throw new Error(`Error - ${response.status}`);
+  }
+  return response.json();
+};
+
+export const getPromo = async (id) => {
+  const response = await fetch(`${process.env.REACT_APP_BASE_URL}/api${id}`);
+  if (!response.ok) {
+    throw new Error(`Error - ${response.status}`);
+  }
+  return response.json();
+};
